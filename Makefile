adf90a0088922218fd96f8c25dc6ed62e78b7a35
Quentin Geissmann <qgeissmann@gmail.com>
2017-08-23 11:57:21 +0100

    test autodeployment and building with bookdown

--- Makefile
+++
@@ -1,7 +0,0 @@
-all : README.md
-
-README.md: README.Rmd
-	@echo "library(knitr); knit('README.Rmd')" | R --vanilla
-clean:
-	rm README.md
-
---
+++ _deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "source" ] && exit 0
+
+git config user.name "rapporter-travis"
+git config user.email "travis"
+
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Automatic deployment after $TRAVIS_COMMIT [ci skip]" || true
+git push -q origin master
